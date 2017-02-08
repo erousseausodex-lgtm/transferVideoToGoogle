a605dc094117afa48881f8d29c0c3eff6361708a
System <none>
2017-02-08 14:36:00 +0000

    Checkpoint

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+* create an app and get your own damn keys 
+* on https://apps.twitter.com/ <3
+* then rename this file to config.js
+*/
+
+// twitter app stuff
+export const twitterAPI = {
+  consumer_key: '',
+  consumer_secret: '',
+  callback_url: 'http://127.0.0.1:3000/login/twitter/return',
+};
--- server.js
+++ server.js
@@ -5,11 +5,31 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// set up twitter passport for oauth
+var twitterAPI = require('./config');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
+var username = '';
+
+passport.use(new Strategy({
+  consumerKey: twitterAPI.consumer_key,
+  consumerSecret: twitterAPI.consumer_secret,
+  callbackURL: twitterAPI.callback_url
+},
+function(token, tokenSecret, profile, cb) {
+  return cb(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
