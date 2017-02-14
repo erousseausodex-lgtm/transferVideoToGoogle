680247a923afcbe4a3d042e6db96b00df8324a2b
System <none>
2017-02-14 00:48:38 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -25,9 +25,15 @@ passport.deserializeUser(function(obj, done) {
 // init project
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 var connect = require('connect-ensure-login');
 
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.use(express.static('public'));
 app.use(expressSession({ secret:'watchingferries', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -38,10 +44,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/success',
-  connect.ensureLoggedIn(),
+app.get('/logoff',
   function(req, res) {
-    res.sendFile(__dirname + '/views/success.html');
+    res.clearCookie('twitter-passport-example');
+    res.redirect('/');
   }
 );
 
@@ -50,7 +56,15 @@ app.get('/login/twitter', passport.authenticate('twitter'));
 app.get('/login/twitter/return', 
   passport.authenticate('twitter', { failureRedirect: '/' }),
   function(req, res) {
+    res.cookie('twitter-passport-example')
     res.redirect('/success');
+  }
+);
+
+app.get('/success',
+  connect.ensureLoggedIn(),
+  function(req, res) {
+    res.sendFile(__dirname + '/views/success.html');
   }
 );
 
