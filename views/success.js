5593797961f9ac19ed044ba618dc6124dfa4db27
System <none>
2017-05-04 16:26:46 +0000

    Checkpoint

--- views/success.js
+++ views/success.js
@@ -5,7 +5,7 @@ $(function() {
     } else {
       $('<span></span>').text("Oh, hi " + datum[0] + "! I have that data for you: ").appendTo('p#deets');
       $('<div></div>').text(datum[1][1]).appendTo('p#deets');
-      $('p#deets').prepend('<img src="//chart.googleapis.com/chart?cht=lc&chtt=Data&chs=250x150&chd=t:'+datum[1][1]+'&chxt=x,y&chxs=0,c0c0c0,10,0,lt|1,c0c0c0,10,1,lt&chco=000000" />');
+      $('p#deets').append('<br /><img src="//chart.googleapis.com/chart?cht=lc&chtt=Data&chs=250x150&chd=t:'+datum[1][1]+'&chxt=x,y&chxs=0,c0c0c0,10,0,lt|1,c0c0c0,10,1,lt&chco=000000" />');
     }
   });
 });
