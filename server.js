42d862871f56b1411409b8ab0ff19303f494be74
Glitch (google-sheets) <none>
2023-12-08 10:49:43 +0000

    🎂😬 Checkpoint ./server.js:41421967/541

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const fs = require('fs');
 const readline = require('readline');
+const {google}= require('googleapis');
+
+const KEYFILEPATH = 'C:\Users\33687\Downloadsclassroomstore-7507cf2dd39f'
+      
+      const SCOPES = ['https://www.googleapis.com/auth/drive'];
+const auth = new google.auth.googleAuth(opt:{
+                                        keyFile:KEYFILEPATH,
+                                        scopes:SCOPES
+                                        });
+aync function createAndUploadFile(auth){
+  const drive
+}
