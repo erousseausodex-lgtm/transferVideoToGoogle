8bfc834689bc63ea901b299b09ff29cf484630e4
Glitch (google-sheets) <none>
2023-12-08 12:42:12 +0000

    🤗👣 Checkpoint ./server.js:41421967/577 ./.glitch-assets:41421967/420

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { google } = require('googleapis');
 const path = require('path');
+const axios = require('axios');
 
 
 const KEYFILEPATH = 'C:\\Users\\33687\\Downloads\\classroomstore-7507cf2dd39f';
@@ -17,18 +18,23 @@ async function createAndUploadFile(auth) {
     auth
   });
   
-  const desktopFilePath = 'C:\\Users\\33687\\Pictures\\COVID\\logo.png'; // Corrected path
-
+  // const desktopFilePath = 'C:/Users/33687/Pictures/COVID/logo.png'; // Corrected path
+ const assetUrl = 'https://cdn.glitch.global/151b8a04-c447-4677-aa3e-8e3bb0c22fe5/logo.png?v=1702023343969';
+ 
+try{ 
+  
+  const fileContent = fs.readFileSync(assetUrl, 'utf-8'); 
   let fileMetaData = {
+    
     name: 'logo.png'
   };
 
   let media = {
     mimeType: 'image/png',
-     body: fs.readFileSync(desktopFilePath)
+     body: fileContent
   };
 
-  try {
+
     let response = await driveService.files.create({
       resource: fileMetaData,
       media: media,
