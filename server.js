b3d7895e3674862aba2ae25cae1f6f8355029454
Glitch (google-sheets) <none>
2023-12-08 11:25:55 +0000

    👹🏓 Checkpoint ./server.js:41421967/2207

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const fs = require('fs');
-const readline = require('readline');
-const {google}= require('googleapis');
+const { google } = require('googleapis');
+const path = require('path');
 
-const KEYFILEPATH = 'C:\Users\33687\Downloadsclassroomstore-7507cf2dd39f'
-      
-      const SCOPES = ['https://www.googleapis.com/auth/drive'];
-const auth = new google.auth.GoogleAuth(opt:{
-                                        keyFile:KEYFILEPATH,
-                                        scopes:SCOPES
-                                        });
-aync function createAndUploadFile(auth){
-  const driveService = google.drive(options:{version:'v3',auth});
-  
-let fileMetaData = {
-  'name':'logo.png'
-  
-    
+
+const KEYFILEPATH = 'C:\\Users\\33687\\Downloads\\classroomstore-7507cf2dd39f';
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: KEYFILEPATH,
+  scopes: SCOPES
+});
+
+async function createAndUploadFile(auth) {
+  const driveService = google.drive({
+    version: 'v3',
+    auth
+  });
+
+  let fileMetaData = {
+    name: 'logo.png'
+  };
+
+  let media = {
+    mimeType: 'image/png',
+    body: fs.createReadStream(path.join(__dirname,'Assets' ,'logo.png'))
+  };
+
+  try {
+    let response = await driveService.files.create({
+      resource: fileMetaData,
+      media: media,
+      fields: 'id'
+    });
+
+    switch (response.status) {
+      case 200:
+        console.log('File created', response.data.id);
+        break;
+    }
+  } catch (error) {
+    console.error('Error creating file:', error.message);
+  }
 }
 
-let media = {
-  mimeType:'logo.png',
-  body:fs.createReadStream(path:'logo.png')
-}
+// Call the function with the auth object
+createAndUploadFile(auth);
 
-let response = await driveService.files.create(params:{
-      ressource:  fileMetaData,
-           media:media,
-          fields :'id'
-                                               
-         })
-switch(response.status){
-    code 200;
-    console.log('File created',response.data.id);
-    break;
-}
-}
 // node server.js
