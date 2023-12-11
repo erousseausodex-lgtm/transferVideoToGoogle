10e5ad500590ab90d227d05dccc0ab8f5d3d7b5c
Glitch (google-sheets) <none>
2023-12-11 13:40:54 +0000

    🍘📒 Checkpoint ./server.js:41421967/315

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const { google } = require('googleapis');
-//const stream = require('stream');
+const stream = require('stream');
 
-const fs = require('fs');
+//const fs = require('fs');
 
 const KEYFILEPATH = 'classroomstore-7507cf2dd39f.json';
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
@@ -22,7 +22,7 @@ async function createAndUploadFile(auth) {
   const assetUrl = 'https://cdn.glitch.global/151b8a04-c447-4677-aa3e-8e3bb0c22fe5/logo.png?v=1702023343969';
 
   try {
-    const response = await axios.get(assetUrl, { responseType: 'arraybuffer' });
+    const response = await axios.get(assetUrl, { responseType: 'stream' });
 
     let fileMetaData = {
       name: 'logo.png',
@@ -31,8 +31,8 @@ async function createAndUploadFile(auth) {
 
      const media = {
       mimeType: 'image/png',
-     // body: response.data.pipe(new stream.PassThrough())
-       body: Buffer.from(response.data, 'binary') 
+     
+      body: response.data.pipe(new stream.PassThrough())
        
     };
 
@@ -47,18 +47,22 @@ async function createAndUploadFile(auth) {
     console.log('File URL:', driveResponse.data.webViewLink);
     console.log('File name:',driveResponse.data.name);
     
+     process.exit(); // Terminate the script after successful execution
+
  
 
    
     
   } catch (error) {
     console.error('Error creating file:', error.message);
+     process.exit(1); // Terminate the script with an error code
+
     
   }
 
 }
 
-createAndUploadFile(auth);
+//createAndUploadFile(auth);
 
 
 // node server.js
