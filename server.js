105f2c1ac85e7746fd1d0d6bad063e246be5d83e
Glitch (google-sheets) <none>
2023-12-11 12:04:52 +0000

    🌈🎆 Checkpoint ./server.js:41421967/339

--- server.js
+++ server.js
@@ -20,42 +20,45 @@ async function createAndUploadFile(auth) {
   const assetUrl = 'https://cdn.glitch.global/151b8a04-c447-4677-aa3e-8e3bb0c22fe5/logo.png?v=1702023343969';
 
   try {
-//     const response = await axios.get(assetUrl, { responseType: 'stream' });
+    const response = await axios.get(assetUrl, { responseType: 'stream' });
 
-//     let fileMetaData = {
-//       name: 'logo.png',
-//       parents:['14D_ANHPoaMvTn5ERk1lrwKW3xEz0_XFs']
-//     };
+    let fileMetaData = {
+      name: 'logo.png',
+      parents:['14D_ANHPoaMvTn5ERk1lrwKW3xEz0_XFs']
+    };
 
-//      const media = {
-//       mimeType: 'image/png',
-//       body: response.data.pipe(new stream.PassThrough())
-//     };
+     const media = {
+      mimeType: 'image/png',
+      body: response.data.pipe(new stream.PassThrough())
+    };
 
-//     let driveResponse = await driveService.files.create({
-//       requestBody: fileMetaData,
-//       media: media,
-//       fields: 'id, webViewLink'
-//     });
+    let driveResponse = await driveService.files.create({
+      requestBody: fileMetaData,
+      media: media,
+      fields: 'id, webViewLink'
+    });
     
-//     const fileMetadata = await driveService.files.get({
-//   fileId: driveResponse.data.id,
-//   fields: 'id, name, parents, webViewLink'
-// });
+    const fileMetadata = await driveService.files.get({
+  fileId: driveResponse.data.id,
+  fields: 'id, name, parents, webViewLink'
+});
 
-// console.log('File Metadata:', fileMetadata.data);
+console.log('File Metadata:', fileMetadata.data);
+
     
     
 
-//     console.log('File created with ID:', driveResponse.data.id);
-//     console.log('File URL:', driveResponse.data.webViewLink);
-//     console.log('File name:',driveResponse.data.name);
-//   } catch (error) {
-//     console.error('Error creating file:', error.message);
-//   }
-// }
+    console.log('File created with ID:', driveResponse.data.id);
+    console.log('File URL:', driveResponse.data.webViewLink);
+    console.log('File name:',driveResponse.data.name);
+    
+   
+  // } catch (error) {
+  //   console.error('Error creating file:', error.message);
+  // }
+}
 
-// // Call the function with the auth object
-// createAndUploadFile(auth)
+// Call the function with the auth object
+createAndUploadFile(auth)
 
 // // node server.js
