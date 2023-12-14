ca24d9981adbc179f7fe0b770503a83b81b64de3
Glitch (google-sheets) <none>
2023-12-14 15:05:38 +0000

    🔨☕️ Checkpoint ./server.js:41421967/7378

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const multer = require('multer');
-const { google } = require('googleapis');
-const fs = require('fs');
-const stream = require('stream');
-const sheets = google.sheets('v4');
+const path = require("path");
+const multer = require("multer");
+const { google } = require("googleapis");
+const fs = require("fs");
+const stream = require("stream");
+const sheets = google.sheets("v4");
 
 // Use a closure to store shared data
 const sharedData = {
@@ -15,131 +15,118 @@ const sharedData = {
 
 // traitement de s paramètres url(rownumber param)
 
-app.get('/user', (req, res) => {
+app.get("/user", (req, res) => {
   // Access parameters from the URL using req.query
-sharedData.rowNumber = req.query.rowNumber;
-   console.log('Received parameters:',sharedData.rowNumber);
-// res.send('Received parameters: ' + rowNumber);
- 
-
+  sharedData.rowNumber = req.query.rowNumber;
+  console.log("Received parameters:" ,sharedData.rowNumber);
+  // res.send('Received parameters: ' + rowNumber);
 });
 // traitement du fichier reçu de index.html
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const upload = multer(); // Initialize multer for handling file uploads
 
-app.post('/upload', upload.single('file'), async (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   const file = req.file;
-  
-   console.log('Received file:', file);
-  
-  const keyFilePath = process.env.GOOGLE_KEY_FILE_PATH ;
+
+  console.log("Received file:", file);
+
+  const keyFilePath = process.env.GOOGLE_KEY_FILE_PATH;
 
   const auth = new google.auth.GoogleAuth({
-    
-    
- 
-    keyFile:keyFilePath,
-    scopes: ['https://www.googleapis.com/auth/drive.file'],
+    keyFile: keyFilePath,
+    scopes: ["https://www.googleapis.com/auth/drive.file"],
   });
 
   try {
     const driveService = google.drive({
-      version: 'v3',
+      version: "v3",
       auth,
     });
 
     const fileMetaData = {
-      name: 'recorded-video.webm',
-      parents: ['14D_ANHPoaMvTn5ERk1lrwKW3xEz0_XFs'], // Replace with your folder ID
+      name: "recorded-video.webm",
+      parents: ["14D_ANHPoaMvTn5ERk1lrwKW3xEz0_XFs"], // Replace with your folder ID
     };
-    
-    
+
     // Create a readable stream from the file buffer
     const fileStream = new stream.PassThrough();
     fileStream.end(file.buffer);
 
     const media = {
-      mimeType: 'video/webm',
+      mimeType: "video/webm",
       //body: file.buffer, // Use the buffer of the uploaded file
-       body: fileStream,
+      body: fileStream,
     };
 
     const driveResponse = await driveService.files.create({
       requestBody: fileMetaData,
       media: media,
-      fields: 'id, webViewLink',
+      fields: "id, webViewLink",
     });
 
-    console.log('File created with ID:', driveResponse.data.id);
-    console.log('File URL:', driveResponse.data.webViewLink);
-    console.log('File name:', driveResponse.data.name);
-    
-     // Store fileData in sharedData
+    console.log("File created with ID:", driveResponse.data.id);
+    console.log("File URL:", driveResponse.data.webViewLink);
+    console.log("File name:", driveResponse.data.name);
+
+    // Store fileData in sharedData
     sharedData.fileData = {
-      webViewLink:  driveResponse.data.webViewLink
+      webViewLink: driveResponse.data.webViewLink,
       // Add other relevant file data properties as needed
     };
-    
-   // Add the file information to the Google Sheet
-    
-  
-    await updateGoogleSheet(sharedData)
+
+    // Add the file information to the Google Sheet
+
+    await updateGoogleSheet(sharedData);
 
     res.json({ fileId: driveResponse.data.id });
-    
-    
   } catch (error) {
-    console.error('Error creating file:', error.message);
-    res.status(500).json({ error: 'Error creating file on Google Drive500' });
+    console.error("Error creating file:", error.message);
+    res.status(500).json({ error: "Error creating file on Google Drive500" });
   }
 });
-
-
 
 // add webviewlink to last row
 async function updateGoogleSheet(sharedDAta) {
   const keyFilePath = process.env.GOOGLE_KEY_FILE_PATH;
 
   const auth = new google.auth.GoogleAuth({
-    keyFile: keyFilePath, 
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    keyFile: keyFilePath,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
-  
-
 
   const sheetsService = google.sheets({
-    version: 'v4',
+    version: "v4",
     auth,
   }).spreadsheets.values;
   const rowNb = sharedData.rowNumber;
 
-  const spreadsheetId = '15qWfOkfmpYaHteMxghAhJtJjYKX8NZWB8j4LBz3ifzU';
-  const range = "'reportage Video'!A"+rowNb+":B"+rowNb; // Adjust the range as needed
+  const spreadsheetId = "15qWfOkfmpYaHteMxghAhJtJjYKX8NZWB8j4LBz3ifzU";
+  const range = "'reportage Video'!A" + rowNb + ":B" + rowNb; // Adjust the range as needed
 
+  const values = [[sharedData.fileData.webViewLink, "videoFile"]];
 
-  const values = [
-    [ sharedData.webViewLink, "videoFile"]
-    ];
-  
- await sheetsService.append({
+  await sheetsService.append({
     spreadsheetId,
     range,
-    valueInputOption: 'RAW',
+    valueInputOption: "RAW",
     resource: {
       values,
     },
   });
 
-  console.log('File information added to Google Sheet.');
+  console.log("File information added to Google Sheet.");
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+console.log("sharedData :",sharedData);
+
+// chargement du fichier
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/views/index.html"));
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
-
