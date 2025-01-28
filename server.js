ffd42f6e4d1e952add7a19b76f0ff12786cf94b5
Glitch (google-sheets) <none>
2025-01-28 11:12:30 +0000

    💑🏌 Checkpoint ./server.js:41421967/585 ./public/views/index.html:41421967/552

--- server.js
+++ server.js
@@ -1,88 +1,18 @@
-// // serve satic html
-// const express = require('express');
-// const app = express();
-// const path = require('path');
+// server.js
+const express = require("express");
+const path = require("path");
+const app = express();
 
-// app.use(express.static('public'));
+// 1) Serve a static "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/views/index.html'));
-// });
+// 2) Simple test route
+app.get("/test", (req, res) => {
+  res.send("Server says: Hello!");
+});
 
-// const listener = app.listen(process.env.PORT || 3000, () => {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
-
-// const axios = require('axios');
-// const { google } = require('googleapis');
-const stream = require('stream');
-
-// //const fs = require('fs');
-
-// const KEYFILEPATH = 'classroomstore-7507cf2dd39f.json';
-// const SCOPES = ['https://www.googleapis.com/auth/drive'];
-
-// const auth = new google.auth.GoogleAuth({
-//   keyFile: KEYFILEPATH,
-//   scopes: SCOPES
-// });
-
-
-
-// async function createAndUploadFile(auth) {
-//   const driveService = google.drive({
-//     version: 'v3',
-//     auth
-//   });
-
-//   // URL of the 'logo.png' asset
-//   const assetUrl = 'https://cdn.glitch.global/151b8a04-c447-4677-aa3e-8e3bb0c22fe5/logo.png?v=1702023343969';
-
-//   try {
-//     const response = await axios.get(assetUrl, { responseType: 'stream' });
-
-//     let fileMetaData = {
-//       name: 'logo.png',
-//       parents:['14D_ANHPoaMvTn5ERk1lrwKW3xEz0_XFs']
-//     };
-
-//      const media = {
-//       mimeType: 'image/png',
-     
-//       body: response.data.pipe(new stream.PassThrough())
-       
-//     };
-
-//     let driveResponse = await driveService.files.create({
-//       requestBody: fileMetaData,
-//       media: media,
-//       fields: 'id, webViewLink'
-    
-//     });
-
-//     console.log('File created with ID:', driveResponse.data.id);
-//     console.log('File URL:', driveResponse.data.webViewLink);
-//     console.log('File name:',driveResponse.data.name);
-    
-//      process.exit(); // Terminate the script after successful execution
-
- 
-
-   
-    
-//   } catch (error) {
-//     console.error('Error creating file:', error.message);
-//      process.exit(1); // Terminate the script with an error code
-
-    
-//   }
-
-// }
-// // console.log('Script started at', new Date());
-// // //createAndUploadFile(auth);
-
-// // console.log('Script ended at', new Date());
-
-
-// // node server.js
-
+// 3) Start listening
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
